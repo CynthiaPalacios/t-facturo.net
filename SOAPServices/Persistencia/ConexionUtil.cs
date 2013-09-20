b493567b7c91adda5d1eb2d6aac6440748dcaf70
Cynthia Palacios <cynthia.pcuadros@gmail.com>
2013-09-19 20:43:23 -0500

    Primer Commit

---
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAPServices.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "Data Source=027894dd-760b-47cf-aebe-a23e0009c55e.sqlserver.sequelizer.com;Initial Catalog=db027894dd760b47cfaebea23e0009c55e;User ID=sfvcgwnsfzdyhwuv ;Password=UvZYu8H7MjqFyVdkPtBpaNgUJE4G38n2WLMJ6hcSL3BfLasz36r8yGXgofpWebdM;  Integrated Security=SSPI;";
+        }
+
+    }
+}
+
